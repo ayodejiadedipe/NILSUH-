9a357e2bd0548eb19d177051e43349620e7f5342
O-Huslin <46905003+O-Huslin@users.noreply.github.com>
2021-04-07 19:49:43 -0500

    Add files via upload

--- login/src/NILSUH/agriculture/Transaction.java
+++ login/src/NILSUH/agriculture/Transaction.java
@@ -1,261 +1,212 @@
-import java.sql.Date;
 import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 //instance of variables
 public class Transaction {
-	private String name;
 	private String type;
-	private int transNum;
-	private Date entryDate;
-	private double amount;
-	private double remain;
-	private String purpose;
-	private int quanNum;
-	private double quanPound;
-	private String source;
-	private String destination;
-	private String Description;
+	private int transNum=0;
+	private Double remain;
+	private LocalDate entryDate;
+	private String dateOpen;
+	private String dateClose;
 	private int status;
-	private Date dateOpen;
-	private Date dateClose;
+	private Double  amount;
+	private int purpose;
+	private Double quanPound;
+	private int quanNum;
+	private String fr;
+	private String t_o;
+	private String description;
+
 
 	// the Transaction constructor
 
-	public Transaction(String name, String type, int transNum, Date entryDate, double amount, double remain,
-			String purpose, int quanNum, double quanPound, String source, String destination, String Description,
-			int status, Date dateOpen, Date dateClose) {
-		this.name = name;
+	public Transaction(String type, Double amount, int purpose, Double quanPound, int quanNum, String fr, String t_o, String description) {
 		this.type = type;
-		this.transNum = transNum;
-		this.entryDate = entryDate;
-		this.amount = amount;
-		this.remain = remain;
+		this.transNum = transNum + 1;
 		this.purpose = purpose;
-		this.quanNum = quanNum;
+
+		if(purpose == 0 || purpose == 5){
+			this.remain = amount + remain;
+		}
+		else{
+			this.remain = remain - amount;
+		}
+
+		this.entryDate = java.time.LocalDate.now();
+		this.status = 0;
+		this.amount = amount;
 		this.quanPound = quanPound;
-		this.source = source;
-		this.destination = destination;
-		this.Description = Description;
-		this.status = status;
-		this.dateOpen = dateOpen;
-		this.dateClose = dateClose;
+		this.quanNum = quanNum;
+		this.fr = fr;
+		this.t_o = t_o;
+		this.description = description;
 	}
 
-	public Transaction(String name2, String type2, int transNum2, int i, double amount2, double remain2,
-			String purpose2, int quanNum2, double quanPound2, String source2, String destination2, String description2,
-			int status2, int j, int k) {
-	}
-
-	// getters and setters
-	String getName() {
-		return name;
-	}
-
-	String getType() {
+	// get type of transaction
+	public String getType() {
 		return type;
 	}
 
-	double getAmount() {
-		return amount;
-	}
-
-	void setType(String newtype) {
-		this.type = newtype;
-	}
-
-	void setAmount(Double amount) {
-		this.amount = amount;
-	}
-
-	/**
-	 * int return the transfer number
-	 */
-	public int getTransNum() {
+	//get transaction ID#
+	public int getId(){
 		return transNum;
 	}
 
-	/**
-	 * transNum the transNum to set
-	 */
-	public void setTransNum(int transNum) {
-		this.transNum = transNum;
-	}
-
-	/**
-	 * Date return the date
-	 */
-	public Date getEntryDate() {
-		return entryDate;
-	}
-
-	/**
-	 * date the date to set
-	 */
-	public void setEntryDate(Date entryDate) {
-		this.entryDate = entryDate;
-	}
-
-	/**
-	 * double return the remain
-	 */
-	public double getRemain() {
+	//get remaining balance 
+	public Double getBalance(){
 		return remain;
 	}
 
-	/**
-	 * remain the remain to set
-	 */
-	public void setRemain(double remain) {
-		this.remain = remain;
+	//get date recorded
+	public LocalDate getEntryDate(){
+		return entryDate;
 	}
 
-	/**
-	 * String return the purpose
-	 */
-	public String getPurpose() {
-		return purpose;
+	//sets the open date of the transaction
+	public void setOpenDate(String op){
+		try{
+			int d = Integer.parseInt(op);
+			
+		}
+		catch(Exception e){
+			System.out.println("Invalid format or values. Please enter in the format MM/DD/YYY");
+		}
 	}
 
-	/**
-	 * purpose the purpose to set
-	 */
-	public void setPurpose(String purpose) {
-		this.purpose = purpose;
-	}
-
-	/**
-	 * int return the quantity Numbers
-	 */
-	public int getQuanNum() {
-		return quanNum;
-	}
-
-	/**
-	 * quantity Number the quantity Number to set
-	 */
-	public void setQuanNum(int quanNum) {
-		this.quanNum = quanNum;
-	}
-
-	/**
-	 * double return the quanPound
-	 */
-	public double getQuanPound() {
-		return quanPound;
-	}
-
-	/**
-	 * quanPound the quanPound to set
-	 */
-	public void setQuanPound(double quanPound) {
-		this.quanPound = quanPound;
-	}
-
-	/**
-	 * String return the source
-	 */
-	public String getSource() {
-		return source;
-	}
-
-	/**
-	 * source the source to set
-	 */
-	public void setSource(String source) {
-		this.source = source;
-	}
-
-	/**
-	 * String return the destination
-	 */
-	public String getDestination() {
-		return destination;
-	}
-
-	/**
-	 * destination the destination to set
-	 */
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
-
-	/**
-	 * String return the Description
-	 */
-	public String getDescription() {
-		return Description;
-	}
-
-	/**
-	 * Description the Description to set
-	 */
-	public void setDescription(String Description) {
-		this.Description = Description;
-	}
-
-	/**
-	 * boolean return the status
-	 */
-	public int getStatus() {
-		return status;
-	}
-
-	/**
-	 * status the status to set
-	 */
-	public void setStatus(int status) {
-		this.status = status;
-	}
-
-	/**
-	 * Date return the dateOpen
-	 */
-	public Date getDateOpen() {
+	//get the date the transaction was done
+	public String getDateOpen(){
 		return dateOpen;
 	}
 
-	/**
-	 * dateOpen the dateOpen to set
-	 */
-	public void setDateOpen(Date dateOpen) {
-		this.dateOpen = dateOpen;
+	//set the closing date of transaction
+	public void setCloseDate(String cl){
+		SimpleDateFormat close = new SimpleDateFormat("MM/dd/yyyy");
+		LocalDate test = null;
+		try{
+			dateClose = new SimpleDateFormat("MM/dd/yyyy").parse(cl);
+		}
+		catch(Exception e){
+			System.out.println("Invalid format. Please put in the format MM/DD/YY");
+		}
 	}
 
-	/**
-	 * Date return the dateClose
-	 */
-	public Date getDateClose() {
+	//get the date the transaction was completed and closed 
+	public String getDateClose(){
 		return dateClose;
 	}
 
-	/**
-	 * dateClose the dateClose to set
-	 */
-	public void setDateClose(Date dateClose) {
-		this.dateClose = dateClose;
+	//set the status of the transaction whether it is open or closed
+	public void setStatus(int st){
+		status = st;
 	}
 
-	// This Method is to deposit a specified amount into the account
-	void deposit(Double balance) {
-		this.amount = amount + balance;
+	//gets the status of the transaction whether it is open or closed
+	public String getStatus(){
+		String status_1 = "Open";
+		String status_2 = "Closed";
+		if(status==0){
+			return status_1;
+		}
+		else{
+			return status_2;
+		}
 	}
 
-	// This Method is to withdraw a specified amount from the account
-	void withdrawal(Double balance) {
-		this.amount = amount - balance;
+	//get the amount in questions for the transaction
+	public Double getAmount(){
+		return amount;
 	}
 
-	@Override
-	public String toString() {
-		String result = "";
-		NumberFormat fmt = NumberFormat.getCurrencyInstance();
-		result = "name='" + getName() + "'" + ", type='" + getType() + "'" + ", transNum='" + getTransNum() + "'"
-				+ ", date='" + getEntryDate() + "'" + ", amount='" + fmt.format(getAmount()) + "'" + ", remain='"
-				+ fmt.format(getRemain()) + "'" + ", purpose='" + getPurpose() + "'" + ", quanNum='" + getQuanNum()
-				+ "'" + ", quanPound='" + getQuanPound() + "'" + ", source='" + getSource() + "'" + ", destination='"
-				+ getDestination() + "'" + ", Description='" + getDescription() + "'" + ", status='" + getStatus() + "'"
-				+ ", dateOpen='" + getDateOpen() + "'" + ", dateClose='" + getDateClose() + "'";
-		return result;
+	//set the purpose of transaction 
+	public void setPurpose(int p){
+		purpose = p;
+	}
+
+	//get the purpose of the transaction 
+	public String getPurpose(){
+		if(purpose == 0){
+			return "Sale";
+		}
+		else if(purpose==1){
+			return "Loan";
+		}
+		else if(purpose==2){
+			return "Credit";
+		}
+		else if(purpose==3){
+			return "Loss";
+		}
+		else if(purpose==4){
+			return "Investment";
+		}
+		else if(purpose==5){
+			return "Loan Repayment";
+		}
+		else if(purpose==6){
+			return "Miscellaneous Fees";
+		}
+		else if(purpose==7){
+			return "Allocation for Banker";
+		}
+		else{
+			return "Unknown purpose";
+		}
+	}
+
+	//set quantity in pound
+	public void setQuanPound(Double pound){
+		quanPound = pound;
+	}
+
+	//gets the quantity in pound 
+	public Double getQuanPound(){
+		return quanPound;
+	}
+
+	//sets the number of chicken sold 
+	public int getQuanNum(){
+		return quanNum;
+	}
+
+	//sets where the transaction source is 
+	public void setSource(String f){
+		fr = f;
+	}
+
+	//gets the source of the transaction 
+	public String getSource(){
+		return fr;
+	}
+
+	//sets the destination of the transaction 
+	public void setTransDestination(String t){
+		t_o = t;
+	}
+
+	//get the destination of the transaction ]
+	public String getDestination(){
+		return t_o;
+	}
+
+	//sets the description of transaction 
+	public void  setDescription(String des){
+		description = des;
+	}
+
+	//get the transaction description 
+	public String getDescription(){
+		return description;
+	}
+
+	//to string method to get the details of the transaction 
+	public String toString(){
+		String trans = "Type: " +  getType() + "\n" + "Transaction ID#: " + getId() + "\n" + "Balance: " + getBalance() + "\n" + "Recorded Date: " + getEntryDate() +  "\n" +
+		"Date Open: " + getDateOpen() + "Date Close: " + getDateClose() + "Status: " + getStatus() + "\n" + "Amount: " + getAmount() + "\n" + 
+		"Purpose: " + getPurpose() + "\n" + "Quantity in Pound(s): " + getQuanPound() + "\n" + "Number of chickes sold: " + getQuanNum() + "\n" + 
+		"Coming From: " + getSource() + "\n" + "Going To: " + getDestination() + "\n" + "Description: " + "\n";
 	}
 
 }
